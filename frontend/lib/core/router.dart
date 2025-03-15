4d3bd0c17db35159ae4f37759fc567226718f094
Thomas Simmer <thomas.simmer@hotmail.fr>
2025-03-15 15:10:59 +0100

    Refactor main in frontend

---
+++ frontend/lib/core/router.dart
@@ -0,0 +1,103 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutteractixapp/core/presentation/root_screen.dart';
+import 'package:flutteractixapp/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:flutteractixapp/features/auth/presentation/bloc/auth_states.dart';
+import 'package:flutteractixapp/features/auth/presentation/screens/login_screen.dart';
+import 'package:flutteractixapp/features/auth/presentation/screens/recover_account_screen.dart';
+import 'package:flutteractixapp/features/auth/presentation/screens/recovery_codes_screen.dart';
+import 'package:flutteractixapp/features/auth/presentation/screens/signup_screen.dart';
+import 'package:flutteractixapp/features/auth/presentation/screens/unauthenticated_home_screen.dart';
+import 'package:flutteractixapp/features/challenges/presentation/challenges_screen.dart';
+import 'package:flutteractixapp/features/habits/presentation/habits_screen.dart';
+import 'package:flutteractixapp/features/messages/presentation/messages_screen.dart';
+import 'package:flutteractixapp/features/profile/presentation/screen/profile_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => UnauthenticatedHomeScreen(),
+      redirect: (context, state) {
+        final authState = context.read<AuthBloc>().state;
+
+        if (authState is AuthAuthenticated) {
+          return '/home';
+        }
+        return null;
+      },
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginScreen(),
+      redirect: (context, state) {
+        final authState = context.read<AuthBloc>().state;
+        if (authState is AuthAuthenticated) {
+          return '/home';
+        }
+        return null;
+      },
+    ),
+    GoRoute(
+      path: '/signup',
+      builder: (context, state) => SignupScreen(),
+      redirect: (context, state) {
+        final authState = context.read<AuthBloc>().state;
+        if (authState is AuthAuthenticated) {
+          return '/home';
+        }
+        return null;
+      },
+    ),
+    GoRoute(
+      path: '/recovery-codes',
+      builder: (context, state) => RecoveryCodesScreen(),
+    ),
+    GoRoute(
+      path: '/recover-account',
+      builder: (context, state) => RecoverAccountScreen(),
+      redirect: (context, state) {
+        final authState = context.read<AuthBloc>().state;
+        if (authState is AuthAuthenticated) {
+          return '/home';
+        }
+        return null;
+      },
+    ),
+    ShellRoute(
+      builder: (context, state, child) => RootScreen(child: child),
+      routes: [
+        GoRoute(
+          path: '/home',
+          builder: (context, state) => HabitsScreen(),
+        ),
+        GoRoute(
+          path: '/habits',
+          builder: (context, state) => HabitsScreen(),
+        ),
+        GoRoute(
+          path: '/challenges',
+          builder: (context, state) => ChallengesScreen(),
+        ),
+        GoRoute(
+          path: '/messages',
+          builder: (context, state) => MessagesScreen(),
+        ),
+        GoRoute(
+          path: '/profile',
+          builder: (context, state) => ProfileScreen(),
+        ),
+      ],
+      redirect: (context, state) {
+        final authState = context.read<AuthBloc>().state;
+
+        if (authState is AuthAuthenticated) {
+          return null;
+        } else {
+          return '/';
+        }
+      },
+    ),
+  ],
+);
